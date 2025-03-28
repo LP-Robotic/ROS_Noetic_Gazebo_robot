e2b53eb9b82cbedcf385630cb39cfcca14013167
Poul <liubimov.pasha@mail.ru>
2025-03-28 09:48:05 +0300

    now two links can move

--- ROS/build/catkin_generated/order_packages.py
+++ ROS/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/poul/Desktop/PROGECTS/my_test_manip/ROS/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/poul/Desktop/PROGECTS/my_test_manip/ROS/devel;/opt/ros/noetic'.split(';') if '/home/poul/Desktop/PROGECTS/my_test_manip/ROS/devel;/opt/ros/noetic' != '' else []
--- ROS/build/catkin_generated/setup_cached.sh
+++ ROS/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/poul/Desktop/PROGECTS/my_test_manip/ROS/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/poul/Desktop/PROGECTS/my_test_manip/ROS/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/poul/Desktop/PROGECTS/my_test_manip/ROS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/poul/Desktop/PROGECTS/my_test_manip/ROS/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/poul/Desktop/PROGECTS/my_test_manip/ROS/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/poul/Desktop/PROGECTS/my_test_manip/ROS/src:$ROS_PACKAGE_PATH"
